691c03c26f31bc8c0d3f3ccf021440cb4368454a
Jiban Jyoti Dash <jjdashofficial@gmail.com>
2026-05-13 11:16:51 +0530

    Change baseUrl in quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "jj-dash.github.io/jjdashweb",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
