e5a004901295c862e50d382fe686c2bed638e6dc
Jiban Jyoti Dash <jjdashofficial@gmail.com>
2026-05-13 09:59:33 +0530

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Jiban Jyoti Dash",
+    pageTitle: "JJ Dash",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
